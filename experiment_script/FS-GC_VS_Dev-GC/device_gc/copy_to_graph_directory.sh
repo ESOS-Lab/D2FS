9a51586fc0e56b2d37461da6f72abfe52634fee3
JuwonKim97 <joo97508@gmail.com>
2024-12-27 02:15:28 +0900

    latest experiment_script

--- experiment_script/FS-GC_VS_Dev-GC/device_gc/copy_to_graph_directory.sh
+++ experiment_script/FS-GC_VS_Dev-GC/device_gc/copy_to_graph_directory.sh
@@ -5,7 +5,8 @@ GRAPH_PATH=(../../graph/Fig-1.FS-GC_VS_Dev-GC)
 OLD_DATA=$(find "$GRAPH_PATH" -name "devgc")
 
 if [ -s "$OLD_DATA" ]; then
-	mv $OLD_DATA/* $GRAPH_PATH/devgc_old
+	cp -r $OLD_DATA/* $GRAPH_PATH/devgc_old
+	rm $OLD_DATA/* -rf
 fi
 
 cp -r $1 ${GRAPH_PATH}/devgc/
