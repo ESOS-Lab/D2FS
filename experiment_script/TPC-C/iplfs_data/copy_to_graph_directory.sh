25741955def5f0514a0f7a5a8a9e72dd36a3af96
JuwonKim97 <joo97508@gmail.com>
2024-12-27 02:14:42 +0900

    latest experiment_script

--- experiment_script/TPC-C/iplfs_data/copy_to_graph_directory.sh
+++ experiment_script/TPC-C/iplfs_data/copy_to_graph_directory.sh
@@ -5,7 +5,8 @@ GRAPH_PATH=(../../graph/Fig-14_and_16.Macrobenchmark_throughput_and_latency)
 OLD_DATA=$(find "$GRAPH_PATH" -name "iplfs_TPC-C")
 
 if [ -s "$OLD_DATA" ]; then
-	mv $OLD_DATA/* $GRAPH_PATH/iplfs_TPC-C_old
+	cp $OLD_DATA/* $GRAPH_PATH/iplfs_TPC-C_old -r
+	rm $OLD_DATA/* -rf
 fi
 
 cp -r $1 ${GRAPH_PATH}/iplfs_TPC-C/
--- experiment_script/TPC-C/zns_data/copy_to_graph_directory.sh
+++ experiment_script/TPC-C/zns_data/copy_to_graph_directory.sh
@@ -5,7 +5,8 @@ GRAPH_PATH=(../../graph/Fig-14_and_16.Macrobenchmark_throughput_and_latency)
 OLD_DATA=$(find "$GRAPH_PATH" -name "zns_TPC-C")
 
 if [ -s "$OLD_DATA" ]; then
-	mv $OLD_DATA/* $GRAPH_PATH/zns_TPC-C_old
+	cp $OLD_DATA/* $GRAPH_PATH/zns_TPC-C_old -r
+	rm $OLD_DATA/* -rf
 fi
 
 cp -r $1 ${GRAPH_PATH}/zns_TPC-C/
@@ -16,7 +17,8 @@ GRAPH_PATH3=(../../graph/Fig-17.GC_latency_breakdown)
 OLD_DATA3=$(find "$GRAPH_PATH3" -name "zns_TPC-C")
 
 if [ -s "$OLD_DATA3" ]; then
-	mv $OLD_DATA3/* $GRAPH_PATH3/zns_TPC-C_old
+	cp $OLD_DATA3/* $GRAPH_PATH3/zns_TPC-C_old -r
+	rm $OLD_DATA3/* -rf
 fi
 
 cp -r $1 ${GRAPH_PATH3}/zns_TPC-C/
